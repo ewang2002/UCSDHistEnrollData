52240918083b805b235f0cd6eecabdc2666b7b3c
Edward Wang <37031713+ewang20027@users.noreply.github.com>
2022-04-23 02:29:58 -0700

    April 23, 2022 - update pt 1 (drop deadline w/o W)

--- SP22D/plotconfig.py
+++ SP22D/plotconfig.py
@@ -1,8 +1,18 @@
 CONFIG = {
-    "markers": [],
+    "markers": [
+        {
+            # Deadline is 04/22/2022 @ 11:59pm, but use 04/23/2022 @ 12am
+            # since it'll look nicer
+            'd': ['2022-04-23'],
+            't': 0,
+            'l': 'solid',
+            'c': '#000000',
+            'n': 'Drop (No W) Deadline'
+        }
+    ],
     "settings": {
         "termName": "Spring 2022 Post-Enrollment",
-        "isNormal": True,
+        "isNormal": False,
         "showTotal": False,
         "useEnrolledTtl": True,
         "useMarkers": True,
