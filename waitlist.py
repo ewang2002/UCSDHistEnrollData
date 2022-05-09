81a3b8731194baad14b6aabb642793b91c10ee7a
Edward Wang <37031713+ewang20027@users.noreply.github.com>
2022-05-09 02:38:26 -0700

    waitlist.py now uses enrolled data instead of available

--- waitlist.py
+++ waitlist.py
@@ -11,32 +11,32 @@ filename = f"{term}/overall/{course}.csv" if len(section) == 0 else f"{term}/sec
 # Load the CSV file into a dataframe
 df = pd.read_csv(filename)
 
-prev_available = 0
+prev_enrolled = 0
 prev_waitlist = 0
 max_total = 0
 
 # Iterate over each row in the dataframe
 num_off = 0
 for index, row in df.iterrows():
-    if prev_available == 0 and prev_waitlist == 0:
-        prev_available = row["available"]
+    if prev_enrolled == 0 and prev_waitlist == 0:
+        prev_enrolled = row["enrolled"]
         prev_waitlist = row["waitlisted"]
         continue
 
     # Get 'available', 'waitlisted', 'total'
     time = row["time"]
-    available = int(row['available'])
+    enrolled = int(row['enrolled'])
     waitlisted = int(row['waitlisted'])
     total = int(row['total'])
 
     if total > max_total:
         max_total = total
 
-    if available < prev_available and waitlisted < prev_waitlist:
-        num_off += prev_available - available
-        print(f"[{time}]: {prev_available - available} student(s) got off the waitlist.")
+    if enrolled > prev_enrolled and waitlisted < prev_waitlist:
+        num_off += enrolled - prev_enrolled
+        print(f"[{time}]: {enrolled - prev_enrolled} student(s) got off the waitlist.")
 
-    prev_available = available
+    prev_enrolled = enrolled
     prev_waitlist = waitlisted
 
 print(f"In total, {num_off} student(s) out of {max_total} students got off the waitlist ({round((num_off / max_total) * 100, 2)}%).")
