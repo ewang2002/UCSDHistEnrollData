edad781307eef295dc075cd465ced12a45a48f4a
Edward Wang <37031713+ewang20027@users.noreply.github.com>
2022-05-10 11:27:31 -0700

    May 10, 2022 - update (raw)

--- multplot.sh
+++ multplot.sh
@@ -26,7 +26,7 @@ do
 done
 
 # Define an array containing S122, S222, S322, and SP22D
-terms=("SP22" "S122" "S222" "S322" "SP22D")
+terms=("S122" "S222" "S322")
 
 # Loop through each term
 for term in ${terms[@]}; do
