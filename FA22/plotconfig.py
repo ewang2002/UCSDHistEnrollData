28b7b1cc9b5254f98f102aa2995e38454168825b
Edward Wang <37031713+ewang20027@users.noreply.github.com>
2022-05-21 10:14:29 -0700

    May 21, 2022 - update S122 S222 FA22

---
+++ FA22/plotconfig.py
@@ -0,0 +1,59 @@
+CONFIG = {
+    "markers": [
+        {
+            'd': ['2022-05-20', '2022-05-28'],
+            't': 8,
+            'l': 'dotted',
+            'c': '#e06d34',
+            'n': 'Priorities Start',
+            's': True
+        },
+        {
+            'd': ['2022-05-20', '2022-05-28'],
+            't': 12,
+            'l': 'dashed',
+            'c': '#42cf1b',
+            'n': 'Seniors Start',
+            's': True
+        },
+        {
+            'd': ['2022-05-23', '2022-05-31'],
+            't': 8,
+            'l': 'dashdot',
+            'c': '#11c7d1',
+            'n': 'Juniors Start',
+            's': True
+        },
+        {
+            'd': ['2022-05-24', '2022-06-01'],
+            't': 8,
+            'l': (0, (3, 5, 1, 5, 1, 5)),
+            'c': '#6a26d1',
+            'n': 'Sophomores Start',
+            's': True
+        },
+        {
+            'd': ['2022-05-25', '2022-06-02'],
+            't': 8,
+            'l': (0, (3, 1, 1, 1)),
+            'c': '#e0e342',
+            'n': 'Freshmen Start',
+            's': True
+        },
+        {
+            'd': ['2022-05-27', '2022-06-03'],
+            't': 22,
+            'l': 'solid',
+            'c': '#000000',
+            'n': 'End (FP/SP)',
+            's': True
+        }
+    ],
+    "settings": {
+        "termName": "Fall 2022",
+        "isNormal": True,
+        "showTotal": True,
+        "useEnrolledTtl": True,
+        "useMarkers": True,
+    }
+}
