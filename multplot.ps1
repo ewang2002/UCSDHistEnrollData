d218bc1e665eb33bd908fb23aa78419d632d6f58
Edward Wang <37031713+ewang20027@users.noreply.github.com>
2022-05-31 10:16:58 -0700

    May 31, 2022 - update (datasets)

---
+++ multplot.ps1
@@ -0,0 +1,26 @@
+$terms = @("FA22", "S122", "S222")
+
+git clone https://github.com/ewang2002/UCSDHistEnrollData
+Set-Location UCSDHistEnrollData
+
+foreach ($term in $terms) {
+    Write-Output "================== Processing $term. =================="
+    Write-Output "\tCleaning raw CSVs."
+    python clean_raw_csvs.py $term
+
+    Write-Output "\tCategorizing enroll data."
+    python enroll_data_cleaner.py $term
+
+    Write-Output "\tPlotting overall data."
+    python plot.py $term o
+    Write-Output "\tPlotting section data."
+    python plot.py $term s
+}
+
+# commit
+git add .
+git commit -m (Get-Date -UFormat "%B %d, %Y - update")
+
+# delete
+Set-Location ..
+Remove-Item -r UCSDHistEnrollData
