d90340f28f45930d72f85e2c822ff0e6918dc381
Edward Wang <37031713+ewang20027@users.noreply.github.com>
2022-05-31 10:36:11 -0700

    fixed multplot.ps1 and added dummy readmes

---
+++ S222/cleaned/README.md
@@ -0,0 +1,1 @@
+This README is here so this folder exists on Github.
--- multplot.ps1
+++ multplot.ps1
@@ -1,19 +1,20 @@
 $terms = @("FA22", "S122", "S222")
 
+Write-Output "`tCloning..."
 git clone https://github.com/ewang2002/UCSDHistEnrollData
 Set-Location UCSDHistEnrollData
 
 foreach ($term in $terms) {
     Write-Output "================== Processing $term. =================="
-    Write-Output "\tCleaning raw CSVs."
+    Write-Output "`tCleaning raw CSVs."
     python clean_raw_csvs.py $term
 
-    Write-Output "\tCategorizing enroll data."
+    Write-Output "`tCategorizing enroll data."
     python enroll_data_cleaner.py $term
 
-    Write-Output "\tPlotting overall data."
+    Write-Output "`tPlotting overall data."
     python plot.py $term o
-    Write-Output "\tPlotting section data."
+    Write-Output "`tPlotting section data."
     python plot.py $term s
 }
 
