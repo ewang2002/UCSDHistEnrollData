30b4154fec0e22fc73c5989d1cdf558557765148
Edward Wang <37031713+ewang20027@users.noreply.github.com>
2022-07-01 15:29:06 -0700

    changed some dates for the plots

--- S122/plotconfig.py
+++ S122/plotconfig.py
@@ -5,15 +5,7 @@ CONFIG = {
             't': 8,
             'l': (0, (3, 5, 1, 5, 1, 5)),
             'c': '#6a26d1',
-            'n': 'UCSD Students Start',
-            's': True
-        },
-        {
-            'd': ['2022-04-18'],
-            't': 8,
-            'l': (0, (3, 1, 1, 1)),
-            'c': '#e0e342',
-            'n': 'Others Start',
+            'n': 'Enrollment Period',
             's': True
         },
         {
